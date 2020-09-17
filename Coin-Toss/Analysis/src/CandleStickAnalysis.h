2dfcdc133a20a0cc8d981958bd5b3f98cdeb3cfd
SaintTan <tanhwaisaint@hotmail.com>
2020-09-17 12:54:41 +1000

    redesigning analyst structure

--- Coin-Toss/Analysis/src/CandleStickAnalysis.h
+++ Coin-Toss/Analysis/src/CandleStickAnalysis.h
@@ -1,11 +1,9 @@
 #pragma once
-#include "Analysis.h"
 #include "Stock.h"
-#include <queue>
 
-class CandleStickAnalysis : public Analysis{
-private:
-public:
-protected:
-};
+namespace Analysis {
+	namespace CandleStickAnalysis {
+		
+	}
+}
 
